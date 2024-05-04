50cbbf0cd21eee19647437e3f743bb96481a5fe3
Shunsuke-Namba <Bobin0603@gmail.com>
2024-05-04 16:26:03 +0900

    fix react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Study React
+          React
         </a>
         <a
           href="https://www.google.co.jp/"
