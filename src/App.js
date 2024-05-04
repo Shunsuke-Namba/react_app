e0b0c7a77fa6360544ad0b00f8695139881ac3df
Shunsuke-Namba <Bobin0603@gmail.com>
2024-05-04 16:21:31 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Study React
+        </a>
+        <a
+          href="https://www.google.co.jp/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          なおき
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
